de566853763783e3278339ae3a0fc1fd8758647b
Sanghyuk.Jung <onlymytho@gmail.com>
2017-07-25 01:52:16 +0900

    Update content_item image aspect ratio as 1:1

--- js/content_functions_mobile.js
+++ js/content_functions_mobile.js
@@ -3,7 +3,7 @@ function auto_content_item() {
   var content_item_width = window.getComputedStyle(document.getElementsByClassName('content_item')[0]).getPropertyValue('width');
   console.log(content_item_width)
   for (i=0; i < document.querySelectorAll('.content .content_carousel .content_item .image').length; i++) {
-    document.querySelectorAll('.content .content_carousel .content_item .image')[i].style.height = 'calc(' + content_item_width.split('calc')[1]+'/1.58)'
+    document.querySelectorAll('.content .content_carousel .content_item .image')[i].style.height = content_item_width
   };
   console.log('adjusted');
 }
